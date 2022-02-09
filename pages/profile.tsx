a3d1c95aabff7c8be1ac08778a79e0dd363f5fb9
ClarXo <cheonaru@gmail.com>
2022-02-10 05:52:37 +0900

    style(profile): design applied with profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,16 +3,16 @@ import type { NextPage } from 'next';
 const Profile: NextPage = () => {
   return (
     <div className="py-10 px-4">
-      <div>
-        <div />
-        <div>
-          <span>Steve Jebs</span>
-          <span>Edit profile &rarr;</span>
+      <div className="flex items-center space-x-3">
+        <div className="w-16 h-16 bg-slate-500 rounded-full" />
+        <div className="flex flex-col">
+          <span className="font-medium text-gray-900">Steve Jebs</span>
+          <span className="text-sm text-gray-700 cursor-pointer">Edit profile &rarr;</span>
         </div>
       </div>
-      <div>
-        <div>
-          <div>
+      <div className="mt-10 flex justify-around">
+        <div className="flex flex-col items-center cursor-pointer">
+          <div className="w-14 h-14 text-white bg-orange-500 hover:bg-orange-600 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -28,10 +28,10 @@ const Profile: NextPage = () => {
               ></path>
             </svg>
           </div>
-          <span>판매내역</span>
+          <span className="text-sm mt-2 font-medium text-gray-700">판매내역</span>
         </div>
-        <div>
-          <div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <div className="w-14 h-14 text-white bg-orange-500 hover:bg-orange-600 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -49,8 +49,8 @@ const Profile: NextPage = () => {
           </div>
           <span>구매내역</span>
         </div>
-        <div>
-          <div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <div className="w-14 h-14 text-white bg-orange-500 hover:bg-orange-600 rounded-full flex items-center justify-center">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -69,12 +69,12 @@ const Profile: NextPage = () => {
           <span>관심목록</span>
         </div>
       </div>
-      <div>
-        <div>
-          <div />
+      <div className="mt-12">
+        <div className="flex space-x-4 items-center">
+          <div className="w-12 h-12 rounded-full bg-slate-500" />
           <div>
-            <h4>니꼬</h4>
-            <div>
+            <h4 className="text-sm font-bold text-gray-800">니꼬</h4>
+            <div className="flex items-center">
               <svg
                 className="text-yellow-400 h-5 w-5"
                 xmlns="http://www.w3.org/2000/svg"
@@ -123,12 +123,14 @@ const Profile: NextPage = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="mt-4 text-gray-600 text-sm">
           <p>
-            Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while
-            I&apos;m in a transitional period so I don&apos;t wanna kill you, I wanna help you. But I can&apos;t give
-            you this case, it don&apos;t belong to me. Besides, I&apos;ve already been through too much shit this
-            morning over this case to hand it over to your dumb ass.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
           </p>
         </div>
       </div>
