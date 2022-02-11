ca15341b2a85c634905bb57b47a9d5b30ffd98cd
ClarXo <imp_rove@naver.com>
2022-02-11 13:13:27 +0900

    refactor(floating-button): export floating button into component

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Layout from "../../components/layout";
+import Floating from "../../components/floating-button";
 
 const Live: NextPage = () => {
   return (
@@ -13,7 +14,7 @@ const Live: NextPage = () => {
             </h1>
           </div>
         ))}
-        <button className="fixed hover:bg-orange-500 transition-colors cursor-pointer  bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 border-transparent text-white">
+        <Floating href={"/live/create"}>
           <svg
             className="w-6 h-6"
             fill="none"
@@ -28,7 +29,7 @@ const Live: NextPage = () => {
               d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
             ></path>
           </svg>
-        </button>
+        </Floating>
       </div>
     </Layout>
   );
