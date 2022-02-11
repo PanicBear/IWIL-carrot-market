01dd08876c4e99986bce12c51c265cda33fe4de2
ClarXo <imp_rove@naver.com>
2022-02-11 17:54:54 +0900

    refactor(textarea): extraced common codes with textarea

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Button from "../../components/button";
 import Input from "../../components/input";
 import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const Upload: NextPage = () => {
   return (
@@ -34,20 +35,7 @@ const Upload: NextPage = () => {
           kind="price"
           required
         />
-        <div>
-          <label
-            htmlFor="description"
-            className="mb-1 block text-sm font-medium text-gray-700"
-          >
-            Description
-          </label>
-
-          <textarea
-            id="description"
-            className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
-            rows={4}
-          />
-        </div>
+        <TextArea name="description" label="Description" />
         <Button text="Upload item" />
       </form>
     </Layout>
