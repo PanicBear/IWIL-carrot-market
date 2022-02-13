66432ce7e1e81ce33228356776be238adca5a812
ClarXo <cheonaru@gmail.com>
2022-02-13 18:27:54 +0900

    chore(database): install planetscale & create db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,16 @@
-import type { NextPage } from "next";
-import FloatingButton from "../components/floating-button";
-import Layout from "../components/layout";
-import Item from "../components/item";
+import type { NextPage } from 'next';
+import FloatingButton from '../components/floating-button';
+import Layout from '../components/layout';
+import Item from '../components/item';
 
 const Home: NextPage = () => {
   return (
     <Layout title="홈" hasTabBar>
       <div className="flex flex-col space-y-5 py-10">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Item
-            key={i}
-            id={i}
-            title={"iPhone 14"}
-            price={99}
-            hearts={1}
-            comments={1}
-          />
+          <Item key={i} id={i} title={'iPhone 14'} price={99} hearts={1} comments={1} />
         ))}
-        <FloatingButton href={"/items/upload"}>
+        <FloatingButton href={'/items/upload'}>
           <svg
             className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -26,12 +19,7 @@ const Home: NextPage = () => {
             stroke="currentColor"
             aria-hidden="true"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
           </svg>
         </FloatingButton>
       </div>
