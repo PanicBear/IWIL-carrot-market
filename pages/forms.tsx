80311d1bc24b29f9b1287de878015d9ed7f7b40a
ClarXo <cheonaru@gmail.com>
2022-02-15 06:02:18 +0900

    test(form): react hook form - resgister example

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,45 +1,19 @@
-import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// Better validation
+// Better Errors(set, clear, display)
+// Have control over inputs
+// Dont deal with events
+// Easier Inputs
 
 export default function Forms() {
-  const [username, setUsername] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [formErrors, setFormErrors] = useState<string>('');
-  const [emailError, setEmailError] = useState<string>('');
-
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-  };
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmail(value);
-  };
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-  };
-  const onSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    if (username === '' || email === '' || password === '') {
-      setFormErrors('All fields are required');
-    }
-    if (!email.includes('@')) {
-      setEmailError('email is required');
-    }
-  };
+  const { register } = useForm();
+  console.log(register('name'));
   return (
-    <form onSubmit={onSubmit}>
-      <input value={username} onChange={onUsernameChange} type="text" placeholder="Username" required minLength={5} />
-      <input value={email} onChange={onEmailChange} type="email" placeholder="Email" required />
-      {emailError}
-      <input value={password} onChange={onPasswordChange} type="password" placeholder="Password" required />
+    <form>
+      <input {...register('username')} type="text" placeholder="Username" required minLength={5} />
+      <input {...register('email')} type="email" placeholder="Email" required />
+      <input {...register('password')} type="password" placeholder="Password" required />
       <input type="submit" value="Create Account" />
     </form>
   );
