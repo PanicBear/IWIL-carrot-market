7bcc4c17f1899044a57c3991adc8134e02f64ef6
ClarXo <cheonaru@gmail.com>
2022-02-15 16:55:27 +0900

    test(form): react hook form - custom validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,34 +1,52 @@
 import { FieldErrors, useForm } from 'react-hook-form';
 
-// Better validation
-// Better Errors(set, clear, display)
-// Have control over inputs
-// Dont deal with events
-// Easier Inputs
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch, handleSubmit } = useForm();
-  const onValid = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({ mode: 'onChange' });
+  const onValid = (data: LoginForm) => {
     console.log('inputs validated');
   };
   const onInvalid = (errors: FieldErrors) => {
     console.log(errors);
   };
 
-  console.log(watch('username'));
-  console.log(watch());
+  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
         {...register('username', {
-          required: true,
+          required: 'Username is required',
+          minLength: {
+            value: 5,
+            message: 'The username should be longer than 5 chars',
+          },
         })}
         type="text"
         placeholder="Username"
       />
-      <input {...register('email', { required: true })} type="email" placeholder="Email" />
-      <input {...register('password', { required: true })} type="password" placeholder="Password" />
+      <input
+        {...register('email', {
+          required: 'Email is required',
+          validate: {
+            notGmail: (value) => !value.includes('@gmail.com') || 'No gmails allowed',
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${Boolean(errors.email) ? 'border-red-500' : ''}`}
+      />
+      {errors.email?.message}
+      <input {...register('password', { required: 'Password is required' })} type="password" placeholder="Password" />
       <input type="submit" value="Create Account" />
     </form>
   );
