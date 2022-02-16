f7b0b51ee90fcb5c929bcaec16cc568269fa5366
ClarXo <cheonaru@gmail.com>
2022-02-16 15:58:23 +0900

    refactor(useMutation): fill useMutation with post fetching phrase

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -11,16 +11,20 @@ export default function useMutation(url: string): [(data: any) => void, UseMutat
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
 
-  function mutation(data: any) {}
+  function mutation(data: any) {
+    setLoading(true);
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json().catch(() => {}))
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }
 
   return [mutation, { loading, data, error }];
 }
-
-// setSubmitting(true);
-//     fetch('/api/users/enter', {
-//       method: 'POST',
-//       body: JSON.stringify(data),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     }).then(() => setSubmitting(false));
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -13,5 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method !== 'POST') {
     res.status(401).end();
   }
+  console.log(req.body);
+  // res.json({ ok: true });
   res.status(200).end();
 }
