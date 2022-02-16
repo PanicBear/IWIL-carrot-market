85efc5d31eb9f63d29fd72766a9c68b4bb2df7f0
ClarXo <cheonaru@gmail.com>
2022-02-16 16:21:45 +0900

    refactor(useMutation): shorten useMutation

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -7,12 +7,16 @@ interface UseMutationState {
 }
 
 export default function useMutation(url: string): [(data: any) => void, UseMutationState] {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<undefined | any>(undefined);
-  const [error, setError] = useState<undefined | any>(undefined);
+  const [state, setState] = useState<UseMutationState>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
 
   function mutation(data: any) {
-    setLoading(true);
+    setState((prev) => {
+      return { ...prev, loading: true };
+    });
     fetch(url, {
       method: 'POST',
       headers: {
@@ -21,10 +25,22 @@ export default function useMutation(url: string): [(data: any) => void, UseMutat
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then(setData)
-      .catch(setError)
-      .finally(() => setLoading(false));
+      .then((data) =>
+        setState((prev) => {
+          return { ...prev, data };
+        }),
+      )
+      .catch((error) =>
+        setState((prev) => {
+          return { ...prev, error };
+        }),
+      )
+      .finally(() =>
+        setState((prev) => {
+          return { ...prev, loading: false };
+        }),
+      );
   }
 
-  return [mutation, { loading, data, error }];
+  return [mutation, state];
 }
