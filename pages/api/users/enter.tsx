129ef2bc0d8a3a9ad4d4858dfcb0ee041a1b9749
ClarXo <cheonaru@gmail.com>
2022-02-17 15:30:10 +0900

    refactor(token): connect token if user exists or create user

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,22 +4,18 @@ import { client, withHandler } from '@libs/server/index';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: 'Anonymous',
-      ...payload,
-    },
-    update: {},
-  });
   const token = await client.token.create({
     data: {
       payload: '1234',
       user: {
-        connect: {
-          id: user.id,
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
         },
       },
     },
