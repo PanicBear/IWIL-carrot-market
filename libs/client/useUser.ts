1cb2bbc4fc7ab0fb7de0b8d561354c8fdabab6b0
ClarXo <cheonaru@gmail.com>
2022-02-22 17:31:26 +0900

    refactor(swr): set global option with fetcher

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,10 +2,8 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import useSwr from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-  const { data, error } = useSwr('/api/users/me', fetcher);
+  const { data, error } = useSwr('/api/users/me');
   const router = useRouter();
 
   useEffect(() => {
