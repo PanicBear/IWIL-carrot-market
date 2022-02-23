f19fbb14a1cace4be9b588989ad54f0f9ff00e35
ClarXo <cheonaru@gmail.com>
2022-02-23 23:35:49 +0900

    feat(product table): created product table and updated related table

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -2,7 +2,7 @@ import type { ResponseType } from '@customTypes/index';
 import { client, withApiSession, withHandler } from '@libs/server/index';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handlerFn(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
@@ -15,6 +15,6 @@ async function handlerFn(req: NextApiRequest, res: NextApiResponse<ResponseType>
 export default withApiSession(
   withHandler({
     method: 'GET',
-    handler: handlerFn,
+    handler,
   }),
 );
