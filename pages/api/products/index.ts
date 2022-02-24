7c0c11477acef7602e45b6137ea5a2b49bf00b24
ClarXo <cheonaru@gmail.com>
2022-02-24 16:27:56 +0900

    refactor(api): refactor apis rest compatible

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -3,33 +3,50 @@ import { client, withApiSession, withHandler } from '@libs/server/index';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  const {
-    body: { name, price, description },
-    session: { user },
-  } = req;
-  console.log(user);
-  const product = await client.product.create({
-    data: {
-      name,
-      price: +price,
-      description,
-      imageUrl: 'not implemented yet',
-      user: {
-        connect: {
-          id: user?.id,
+  switch (req.method) {
+    case 'GET':
+      const products = await client.product.findMany();
+      res.json({
+        ok: true,
+        products,
+      });
+      break;
+    case 'POST':
+      const {
+        body: { name, price, description },
+        session: { user },
+      } = req;
+      const product = await client.product.create({
+        data: {
+          name,
+          price: +price,
+          description,
+          imageUrl: 'not implemented yet',
+          user: {
+            connect: {
+              id: user?.id,
+            },
+          },
         },
-      },
-    },
-  });
-  res.json({
-    ok: true,
-    product,
-  });
+      });
+      res.json({
+        ok: true,
+        product,
+      });
+      break;
+    case 'PUT':
+    case 'DELETE':
+    default:
+      res.json({
+        ok: false,
+        message: 'Method not allowed',
+      });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    method: 'POST',
+    methods: ['GET', 'POST'],
     handler,
   }),
 );
