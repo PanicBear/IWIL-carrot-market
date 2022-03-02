aa5453edcfc7e5c28499ff3edd2ce53f55cd49bb
ClarXo <cheonaru@gmail.com>
2022-03-02 17:07:21 +0900

    feat(community): replace dummy with real post list

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -3,29 +3,63 @@ import { client, withApiSession, withHandler } from '@libs/server/index';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  const {
-    body: { question },
-    session: { user },
-  } = req;
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  switch (req.method) {
+    case 'GET':
+      const posts = await client.post.findMany({
+        include: {
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+          _count: {
+            select: {
+              answers: true,
+              wonderings: true,
+            },
+          },
         },
-      },
-    },
-  });
-  res.json({
-    ok: true,
-    post,
-  });
+      });
+      res.json({
+        ok: true,
+        posts,
+      });
+      break;
+    case 'POST':
+      const {
+        body: { question },
+        session: { user },
+      } = req;
+      const post = await client.post.create({
+        data: {
+          question,
+          user: {
+            connect: {
+              id: user?.id,
+            },
+          },
+        },
+      });
+      res.json({
+        ok: true,
+        post,
+      });
+      break;
+    case 'PUT':
+    case 'DELETE':
+    default:
+      res.json({
+        ok: false,
+        message: 'method not allowed',
+      });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['POST'],
+    methods: ['GET', 'POST'],
     handler,
   }),
 );
