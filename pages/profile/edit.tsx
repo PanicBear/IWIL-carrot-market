cd86021abb4f5d6c8c06d08c076471dc75dbf983
ClarXo <cheonaru@gmail.com>
2022-03-08 23:18:05 +0900

    feat(upload image): upload image with cloudinary, not cloudflare

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,13 +1,15 @@
 import { Button, Input, Layout } from '@components/index';
 import { useMutation, useUser } from '@libs/client';
+import useImage from '@libs/client/useImage';
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface EditProfileForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -19,17 +21,27 @@ interface EditProfileResponse {
 const EditProfile: NextPage = () => {
   const { user } = useUser();
   const [editProfile, { loading, data }] = useMutation<EditProfileResponse>('/api/users/me');
+  const [changeAvatar, { loading: avatarLoading, data: avatarData }] = useImage();
   const {
     register,
     handleSubmit,
     setValue,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const [avatarPreview, setAvatarPreview] = useState('');
+  const avatar = watch('avatar');
+
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
-    if (!email && !phone && !name)
+    if (!email && !phone && !name) {
       return setError('formErrors', { message: 'Email or Phone number is required. Choose one, not both.' });
+    }
+    if (avatar && avatar.length) {
+      const file = avatar[0];
+      changeAvatar(file);
+    }
     editProfile({ email, phone, name });
   };
 
@@ -38,23 +50,36 @@ const EditProfile: NextPage = () => {
     setValue('email', user?.email ?? '');
     setValue('phone', user?.phone ?? '');
   }, [user, setValue]);
+
   useEffect(() => {
     if (data && !data.ok) {
       setError('formErrors', { message: data?.error });
     }
   }, [data, setError]);
+  useEffect(() => {
+    if (avatar && avatar.length) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
+
+  console.log(avatarData);
 
   return (
     <Layout title="Edit Profile" canGoBack>
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img src={avatarPreview} className="w-14 h-14 rounded-full bg-slate-500" />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
-            <input id="picture" type="file" className="hidden" accept="image/*" />
+            <input {...register('avatar')} id="picture" type="file" className="hidden" accept="image/*" />
           </label>
         </div>
         <Input register={register('name', { required: true })} name="Name" label="name" type="text" required={true} />
